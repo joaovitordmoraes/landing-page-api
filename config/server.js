06e8bb1516e43b0cebbad2765581255918f21716
Joao Moraes <joaomoraes@DESKTOP-2CTLM1U.localdomain>
2021-04-10 22:29:24 -0300

    Init Strapi project

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '91b294a3f57630a1c43c5dc438b26402'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fff3f982-e922-4414-bd73-0953516a873a'
+};
